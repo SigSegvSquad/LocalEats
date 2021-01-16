d1395a9295958dbbbeeadf477d4a4b12855f1572
Power-PP <priteshpawar05082000@gmail.com>
2021-01-16 21:58:15 +0530

    Menu

--- lib/miscellaneous/Menu.dart
+++ lib/miscellaneous/Menu.dart
@@ -28,7 +28,7 @@ class Menu extends StatelessWidget {
             }),
         title: const Text("Today's Menu"),
       ),
-      body: Center(
+      body: Container(
         child: Container(
             width: double.maxFinite,
             padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
@@ -94,29 +94,94 @@ class MenuTile extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Align(
-              child: Padding(
-                  padding: EdgeInsets.fromLTRB(5, 10, 5, 5),
-                  child: Text(
-                    name,
-                    style: TextStyle(
-                        fontFamily: 'Lobster',
-                        fontWeight: FontWeight.normal,
-                        fontSize: 18,
-                        color: Colors.black),
-                  ))),
-          Align(
-              child: Padding(
-                  padding: EdgeInsets.fromLTRB(5, 10, 5, 5),
-                  child: Text(
-                    price,
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 18,
-                        color: Colors.black),
-                  )))
+          SizedBox(
+            width: 200,
+            child: Column(
+              children: [Align(
+                  child: Padding(
+                      padding: EdgeInsets.fromLTRB(5, 10, 5, 5),
+
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                            fontFamily: 'Lobster',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 22,
+                            color: Colors.black),
+                      ))),
+                Align(
+                    child: Padding(
+                        padding: EdgeInsets.fromLTRB(5, 10, 5, 5),
+                        child: Text(
+                          '₹ '+price, textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 22,
+                              color: Colors.black),
+                        )))],
+            ),
+          ),
+
+          AwesomeButton()
+
         ],
       ),
     ));
   }
+}
+
+class AwesomeButton extends StatefulWidget {
+  @override
+  AwesomeButtonState createState() => new AwesomeButtonState();
+}
+
+class AwesomeButtonState extends State<AwesomeButton> {
+  int counter = 1;
+  @override
+  Widget build(BuildContext context){
+    return  new Container(
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 50.0,
+                      child: new RaisedButton(child: new Text("-", style: new TextStyle(color: Colors.black, fontStyle: FontStyle.italic, fontSize: 30.0)),shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                          side: BorderSide(color: Colors.red)
+                      ),
+                          onPressed: () {
+                            setState(() {
+                              if (counter > 0) (
+                                  // ignore: unnecessary_statements
+                                  counter--
+                              );
+                            });
+                          }
+                      )
+                    ),
+
+                    new Text('   $counter', style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+                    new Padding(padding: new EdgeInsets.all(15.0)),
+                    new SizedBox(
+                      width: 50.0,
+                        child: new RaisedButton(
+                            child: new Text("+", style: new TextStyle(color: Colors.black, fontStyle: FontStyle.normal, fontSize: 30.0)),
+                            shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                                side: BorderSide(color: Colors.red)
+                        ),
+                            onPressed: (){
+                              setState(() {
+                                counter++;
+                              });
+                            }
+                        ),
+                    ),
+
+
+                  ],
+                )
+            );
+
+      }
 }
